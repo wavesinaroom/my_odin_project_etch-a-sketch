48e74edac81c35912c78d6f6170cdd1164b64ebf
Juan Jáuregui <juan@wavesinaroom.com>
2022-04-27 18:04:04 -0500

    [Feature] Reaplace tr and tb with div and set correct size to them

--- sketch.js
+++ sketch.js
@@ -7,22 +7,22 @@ function createGrid()
   //Creates table frame
   const cellsDiv = document.createElement("div");
   cellsDiv.id = 'cells';
-  cellsDiv.innerHTML = "<table id='cellsGrid'></table>";
+  cellsDiv.innerHTML = "<div id='cellsGrid'></div>";
   documentBody.appendChild(cellsDiv, documentBody.firstChild);
 
   //Sets table style
-  let cells = document.getElementById("cells");
-  cells.style.display = "block";
-  cells.style.width = window.innerWidth + "px";
-  cells.style.heigt = window.innerHeight + "px";
 
   let cellsGrid = document.getElementById("cellsGrid");
   const cellsNumber = 16;
+  const cellsWidth = window.innerWidth/cellsNumber;
+  const cellsHeight = window.innerHeight/cellsNumber;
 
   for (var i = 0; i <cellsNumber; i++) {
-    let row = document.createElement('tr');
+    let row = document.createElement('div');
     for (var j = 0; j < cellsNumber; j++) {
-      let cell = document.createElement('td');
+      let cell = document.createElement('div');
+      cell.style.width = cellsWidth + "px";
+      cell.style.height = cellsHeight + "px";
       row.appendChild(cell);
     }
     cellsGrid.appendChild(row);
