dd6f68dcb9c6eed97648f76da3e883818c68ebf2
Juan Jáuregui <juan@wavesinaroom.com>
2022-04-27 17:15:48 -0500

    [Feature] Create a 16 by 16 table dynamically

--- sketch.js
+++ sketch.js
@@ -3,16 +3,30 @@ function createGrid()
 {
   let documentBody = document.body;
 
-  const cells = document.createElement("div");
-  cells.id = 'cells';
-  cells.innerHTML = "<table id='cellsGrid'></table>";
-  documentBody.insertBefore(cells, documentBody.firstChild);
 
+  //Creates table frame
+  const cellsDiv = document.createElement("div");
+  cellsDiv.id = 'cells';
+  cellsDiv.innerHTML = "<table id='cellsGrid'></table>";
+  documentBody.appendChild(cellsDiv, documentBody.firstChild);
 
-  const gridSize = 16;
-  const cellWidth = window.innerWidth/gridSize;
-  const cellHeight = window.innerHeight/gridSize;
+  //Sets table style
+  let cells = document.getElementById("cells");
+  cells.style.display = "block";
+  cells.style.width = window.innerWidth + "px";
+  cells.style.heigt = window.innerHeight + "px";
+
+  let cellsGrid = document.getElementById("cellsGrid");
+  const cellsNumber = 16;
+
+  for (var i = 0; i <cellsNumber; i++) {
+    let row = document.createElement('tr');
+    for (var j = 0; j < cellsNumber; j++) {
+      let cell = document.createElement('td');
+      row.appendChild(cell);
+    }
+    cellsGrid.appendChild(row);
+  }
 }
-
 
 createGrid();
