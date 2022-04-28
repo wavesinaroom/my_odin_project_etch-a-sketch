d6599aa95bdcdfd9d2c0fb7fe508d164f06240f1
Juan Jáuregui <juan@wavesinaroom.com>
2022-04-28 10:43:25 -0500

    [Fix] Remove cells grid element to avoid unnecesary nesting in extra elements

--- sketch.js
+++ sketch.js
@@ -7,12 +7,9 @@ function createGrid()
   //Creates table frame
   const cellsDiv = document.createElement("div");
   cellsDiv.id = 'cells';
-  cellsDiv.innerHTML = "<div id='cellsGrid'></div>";
   documentBody.appendChild(cellsDiv, documentBody.firstChild);
 
   //Sets table style
-
-  let cellsGrid = document.getElementById("cellsGrid");
   const cellsNumber = 16;
   const cellsWidth = window.innerWidth/cellsNumber;
   const cellsHeight = window.innerHeight/cellsNumber;
@@ -23,9 +20,10 @@ function createGrid()
       let cell = document.createElement('div');
       cell.style.width = cellsWidth + "px";
       cell.style.height = cellsHeight + "px";
+      cell.addEventListener("onmouseover", () => {console.log("Mouse in")});
       row.appendChild(cell);
     }
-    cellsGrid.appendChild(row);
+    cellsDiv.appendChild(row);
   }
 }
 
