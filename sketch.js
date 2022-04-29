374bae7ab393813ba926509b812e06f6ff69ecec
Juan Jáuregui <juan@wavesinaroom.com>
2022-04-29 15:56:20 -0500

    [Feature] Button reset grid to specific number from user's input

--- sketch.js
+++ sketch.js
@@ -1,18 +1,15 @@
+let documentBody = document.body;
 
-function createGrid()
+
+function createCells(cellsNumber)
 {
-  let documentBody = document.body;
-
-
-  //Creates table frame
-  const cellsDiv = document.createElement("div");
+  cellsDiv = document.createElement("div");
   cellsDiv.id = 'cells';
-  documentBody.appendChild(cellsDiv, documentBody.firstChild);
+  documentBody.appendChild(cellsDiv);
 
   //Sets table style
-  const cellsNumber = 160;
-  const cellsWidth = window.innerWidth/cellsNumber;
-  const cellsHeight = window.innerHeight/cellsNumber;
+  let cellsWidth = window.innerWidth/cellsNumber;
+  let cellsHeight = window.innerHeight/cellsNumber;
 
   for (var i = 0; i <cellsNumber; i++) {
     let row = document.createElement('div');
@@ -28,4 +25,31 @@ function createGrid()
   }
 }
 
-createGrid();
+function setUpGrid()
+{
+  let cellsDiv = document.getElementById("cells");
+  //if there's a grid and create one otherwise just create one
+  if(cellsDiv == null)
+  {
+    const buttonHolder = document.createElement("div");
+    buttonHolder.id = 'buttonHolder';
+    documentBody.appendChild(buttonHolder);
+
+    //Creates button
+    const promptButton = document.createElement("button");
+    promptButton.id = 'promptButton';
+    promptButton.innerText = "Change cells number";
+    promptButton.addEventListener("click", setUpGrid);
+    buttonHolder.appendChild(promptButton);
+    createCells(prompt("Please enter a positive number up to 100"));
+    //Creates button holder
+
+    //Creates table frame
+  }else{
+    alert("There's a grid in there");
+    cells.remove();
+    createCells(prompt("Please enter a positive number up to 100"));
+  }
+}
+
+setUpGrid();
